1cc65b34da037a6699383207e7d1ee732f65ef97
tahatriqui <120963723+tahatriqui@users.noreply.github.com>
2025-02-04 09:11:49 +0100

    first commit

---
+++ backend/routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+// Static route example
+Route::get('/hello', function () {
+    return response()->json([[
+        'message' => 'Hello, this is a static API response!'
+    ] ,
+[
+        'message' => 'Hello, this is a static API response!'
+    ] ,
+[
+        'message' => 'Hello, this is a static API response!'
+    ] ,
+[
+        'message' => 'Hello, this is a static API response!'
+    ] ]);
+});
